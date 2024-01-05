65d52304b02326c7c3cd0a55420f9c8d622c9be4
OlenaKolesnyk <32517657+OlenaKolesnyk@users.noreply.github.com>
2024-01-05 15:32:56 +0200

    Create LB_3.py

---
+++ LB_3.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+message_1 = input()
+message_2 = input()
+message_3 = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("clear message")
