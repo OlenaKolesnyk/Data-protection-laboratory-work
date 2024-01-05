699850fa241195a61347612a21f39b47a3c0fb29
OlenaKolesnyk <32517657+OlenaKolesnyk@users.noreply.github.com>
2024-01-05 14:46:54 +0200

    Create LB_1.py

---
+++ LB_1.py
@@ -0,0 +1,16 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const operation = readline();
+const pseudoRandomNumber = parseInt(readline());
+for (let i = 0; i < 3; i++) {
+    const rotor = readline();
+}
+const message = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('message');
